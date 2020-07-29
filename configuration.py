b5a2377fc30874b5a4d1b0afabb922cae47eaa0b
Joshua Smeda <smeda.josh14@gmail.com>
2020-07-29 12:21:16 +0000

    Addressing #1

---
+++ configuration.py
@@ -0,0 +1,23 @@
+SYSTEM_SETTINGS = {
+    'HIVE_SERVER_IP': 'hiveurl.domain.com',
+    'HIVE_SERVER_PORT': 9000,
+    'HIVE_URL': 'https://hiveurl.domain.com',
+    'HIVE_API_KEY' : '15015015agmagomag051015',
+    'LOG_FILE_LOCATION': 'thehive_sla_monitor.debug.log',
+    'FLASK_WEBSERVER_IP': '192.168.1.2',
+    'FLASK_WEBSERVER_PORT': 3000,
+}
+
+TWILIO_SETTINGS = {
+    'TWILIO_SENDER': '+000000000',
+    'TWILIO_RTCP': '+0000000000',
+    'ACCOUNT_SID': 'Aagiant0105101501551',
+    'AUTH_TOKEN': '21510515mlagagotg',
+    'TWIMLET_URL': 'http://twimlet.domain.com',
+}
+
+SLACK_SETTINGS = {
+    'SLACK_APP_TOKEN': 'test',
+    'SLACK_CHANNEL' : 'test',
+    'SLACK_WEBHOOK_URL': 'test',
+}
---
+++ thehive_sla_monitor/templates.py
@@ -0,0 +1,113 @@
+import configuration # To import Hive URL
+
+def slack_bot_alert_notice_template(id, rule_name, alert_date, alert_age):
+  data = \
+        [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "A new SLA breach has been raised:\n*<https://" + configuration.SYSTEM_SETTINGS['HIVE_URL'] + "/hive/index.html|The Hive>*"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Alert ID:*\n" + id + "\n*Triggered Rule:*\n" + rule_name + "\n*Alert Created:*\n" + alert_date + "\n*Alert Age:*\n" + alert_age
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_a68ccc480d8ab103641960b1c5fc9fbb/thehive.png",
+            "alt_text": "The Hive Project"
+          }
+        },
+        {
+          "type": "actions",
+          "elements": [
+          {
+            "type": "button",
+            "url": "http://" + configuration.SYSTEM_SETTINGS['HIVE_URL'] + ":3000/web_api/complete/" + id,
+            "text": {
+              "type": "plain_text",
+              "text": "Promote To Case"
+            },
+            "style": "primary"
+          },
+          {
+            "type": "button",
+            "url": "http://" + configuration.SYSTEM_SETTINGS['HIVE_URL'] + ":3000/web_api/ignore/" + id,
+            "text": {
+              "type": "plain_text",
+              "text": "Ignore Case"
+          },
+            "style": "danger",
+          }
+        ]
+        }
+        ]
+  return data
+
+def slack_bot_alert_notice_update(id, rule_name, alert_date, alert_age):
+  data = \
+        [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "A new SLA breach has been raised:\n*<https://" + configuration.SYSTEM_SETTINGS['HIVE_URL'] + "/hive/index.html|The Hive>*"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Alert ID:*\n" + id + "\n*Triggered Rule:*\n" + rule_name + "\n*Alert Created:*\n" + alert_date + "\n*Alert Age:*\n" + alert_age
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_a68ccc480d8ab103641960b1c5fc9fbb/thehive.png",
+            "alt_text": "The Hive Project"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":heavy_check_mark: *Successfully Imported Case!*"
+          }
+        }
+        ]
+  return data
+
+def slack_bot_alert_notice_ignore(id, rule_name, alert_date, alert_age):
+  data = \
+        [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "A new SLA breach has been raised:\n*<https://" + configuration.SYSTEM_SETTINGS['HIVE_URL'] + "/hive/index.html|The Hive>*"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Alert ID:*\n" + id + "\n*Triggered Rule:*\n" + rule_name + "\n*Alert Created:*\n" + alert_date + "\n*Alert Age:*\n" + alert_age + "\n*IR Engineer:* " + "Joshua"
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_a68ccc480d8ab103641960b1c5fc9fbb/thehive.png",
+            "alt_text": "The Hive Project"
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":x: *Case Temporarily Ignored!*"
+          }
+        }
+        ]
+  return data
