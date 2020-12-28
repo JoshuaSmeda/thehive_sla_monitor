042a0a11327948c530c2e5e6e839733f9848390d
JoshuaSmeda <smeda.josh14@gmail.com>
2020-12-28 23:38:15 +0200

    Linting

--- create_alert.py
+++ create_alert.py
@@ -1,22 +1,25 @@
 # This script creates a sample Hive alert. Requires a working Hive instance and API key
 
 import requests
-import configuration
 import random
+import configuration
 
 random.seed(random.randint(1, 1000))
 seed = random.random()
+hive_server_ip = configuration.SYSTEM_SETTINGS['HIVE_SERVER_IP']
+hive_server_port = int(configuration.SYSTEM_SETTINGS['HIVE_SERVER_PORT'])
 
 headers = {
     'Authorization': 'Bearer %s' % configuration.SYSTEM_SETTINGS['HIVE_API_KEY'],
     'Content-Type': 'application/json',
 }
 
-data = '{ "title": "New Alert", "description": "N/A", "type": "external", "source": "instance1", "sourceRef": "%s", "severity": 3 }' % seed
+data = '{ "title": "New Alert", "description": "N/A", "type": "external", "source": \
+        "instance1", "sourceRef": "%s", "severity": 3 }' % seed
 
-response = requests.post('http://%s:%d/api/alert' % (configuration.SYSTEM_SETTINGS['HIVE_SERVER_IP'], configuration.SYSTEM_SETTINGS['HIVE_SERVER_PORT']), headers=headers, data=data)
+response = requests.post('http://%s:%d/api/alert' % (hive_server_ip, hive_server_port), headers=headers, data=data)
 
-if response == 200:
-    print("Successful")
+if response.status_code == 201:
+    print("Hive Alert Successfully Created!")
 else:
     print(response.text)
