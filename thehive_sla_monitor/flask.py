dd53fdb3338137ebf191ab410c20d87d08139db6
JoshuaSmeda <joshua.smeda@xneelo.com>
2020-12-29 11:53:44 +0200

    Linting and fixes

--- thehive_sla_monitor/flask.py
+++ thehive_sla_monitor/flask.py
@@ -1,40 +1,42 @@
+"""
+This module defines the configuration for the Flask webserver.
+Used in conjuction with the Slack intregration.
+"""
+
 import configuration
 
-#from main import promote_to_case
+# Custom Imports
+from flask import Flask, redirect
 from thehive_sla_monitor.logger import logging
-from flask import Flask, request, make_response, redirect
 from thehive_sla_monitor.slack.base import Slack
-from main import promote_to_case
+from thehive_sla_monitor.alerter import ignore_list
+from thehive_sla_monitor.helpers import clean_ignore_list, add_to_temp_ignore, promote_to_case
 
 # Instantiate Flask application
 app = Flask(__name__)
 
-@app.route("/complete/<id>", methods=['GET', 'POST'])
-def complete(id):
-  case_id = promote_to_case(id)
-  #if case_id != None: # Handle if TheHive alert doesn't exist
-  logging.info("ID: %s. Case ID: %s" % (id, case_id))
+@app.route("/complete/<alert_id>", methods=['GET', 'POST'])
+def complete(alert_id):
+    """
+    This method executes a promotion via the Hive for the selected alert_id
+    The original Slack message is also updated
+    The user is redirected to the approriate case via the Hive
+    """
+    case_id = promote_to_case(id)
+    #if case_id != None: # Handle if TheHive alert doesn't exist
+    logging.info("ID: %s. Case ID: %s" % (alert_id, case_id))
+    Slack().slack_chat_update(alert_id)
+    hive_link = "http://%s:%d/index.html#!/case/%s/details" % (configuration.SYSTEM_SETTINGS['HIVE_SERVER_IP'], configuration.SYSTEM_SETTINGS['HIVE_SERVER_PORT'], case_id)
+    return redirect(hive_link, code=302)
 
-  Slack().slack_chat_update(id)
 
-  hive_link = "http://%s:%d/index.html#!/case/%s/details" % (configuration.SYSTEM_SETTINGS['HIVE_SERVER_IP'], configuration.SYSTEM_SETTINGS['HIVE_SERVER_PORT'], case_id)
-  return redirect(hive_link, code=302)
-
-def add_to_temp_ignore(id):
-  logging.info("Adding %s to ignore list" % (id))
-  ignore_list.append(id)
-  ignore_thread = threading.Thread(target=clean_ignore_list)
-  ignore_thread.start()
-  return id
-
-@app.route("/ignore/<id>", methods=['GET', 'POST'])
-def ignore(id):
-  add_to_temp_ignore(id)
-  ignore_id = add_to_temp_ignore(id)
-  logging.info("Ignoring ID: " + ignore_id)
-
-  #res = slack_client.chat.update(channel=alert_dict[id]['channel'], ts=alert_dict[id]["ts"], text="TheHive SLA Monitor: Case Ignored", blocks=slack_bot_alert_notice_ignore(id, alert_dict[id]['rule_name'], alert_dict[id]['alert_date'], alert_dict[id]['alert_age']))
-  #res = slack_client.chat.getPermalink(channel=alert_dict[id]["channel"],message_ts=alert_dict[id]["ts"])
-
-  return ('', 204)
-
+@app.route("/ignore/<alert_id>", methods=['GET', 'POST'])
+def ignore(alert_id):
+    """
+    This method adds the alert_id to the ignore list and updates the original Slack message
+    """
+    add_to_temp_ignore(alert_id)
+    ignore_id = add_to_temp_ignore(alert_id)
+    logging.info("Ignoring ID: %s" % ignore_id)
+    Slack().slack_chat_ignore(alert_id)
+    return ('', 204)
--- thehive_sla_monitor/helpers.py
+++ thehive_sla_monitor/helpers.py
@@ -1,27 +1,74 @@
+"""
+This module provides helper functions that is used throughout this program.
+"""
+import json
+import time as t
 import threading
 
-"""
-This file is responsible for keeping an in-memory track of Hive alerts and there current associated SLA tier.
-"""
-def clean_ignore_list(id):
-  t.sleep(3600)
-  logging.info("Removing " + id + " from ignore list")
-  ignore_list.remove(id)
+# TheHive Imports
+from thehive4py.api import TheHiveApi
 
-def add_to_temp_ignore(id):
-  logging.info("Adding " + id + " to ignore list")
-  ignore_list.append(id)
-  ignore_thread = threading.Thread(target=clean_ignore_list)
-  ignore_thread.start()
-  return id
+# Custom Imports
+import configuration
+from thehive_sla_monitor.logger import logging
+from thehive_sla_monitor.alerter import ignore_list, called_list
 
-def add_to_called_list(id):
-  logging.info("Adding " + id + " to called list")
-  called_list.append(id)
-  return id
+# Defining variables
+
+HIVE_SERVER_IP = configuration.SYSTEM_SETTINGS['HIVE_SERVER_IP']
+HIVE_SERVER_PORT = configuration.SYSTEM_SETTINGS['HIVE_SERVER_PORT']
+HIVE_API_KEY = configuration.SYSTEM_SETTINGS['HIVE_API_KEY']
+HIVE_API = TheHiveApi("http://%s:%s" % (HIVE_SERVER_IP, HIVE_SERVER_PORT), HIVE_API_KEY)
+
+def clean_ignore_list(alert_id):
+    """
+    This method clears the ignore_list after 30 minutes.
+    """
+    t.sleep(3600)
+    logging.info("Removing %s from ignore list" % alert_id)
+    ignore_list.remove(alert_id)
+
+
+def add_to_temp_ignore(alert_id):
+    """
+    This method adds the alert_id to the ignore_list as a thread to avoid being re-alerted on.
+    """
+    logging.info("Adding %s to ignore list" % alert_id)
+    ignore_list.append(alert_id)
+    ignore_thread = threading.Thread(target=clean_ignore_list)
+    ignore_thread.start()
+    return alert_id
+
+
+def add_to_called_list(alert_id):
+    """
+    This method adds the alert_id to the called_list
+    """
+    logging.info("Adding %s to called list" % alert_id)
+    called_list.append(alert_id)
+    return alert_id
+
 
 def is_empty(any_structure):
+    """
+    This method checks whether an object is empty
+    """
     if any_structure:
         return False
+  #  else:
+  #     return True
+
+
+def promote_to_case(case_id):
+    """
+    This method promotes a provided alert to an case via the Hive
+    """
+    logging.info("Promoting Alert %s" % case_id)
+    response = HIVE_API.promote_alert_to_case(case_id)
+    if response.status_code == 201:
+        data = json.dumps(response.json())
+        jdata = json.loads(data)
+        case_id = (jdata['id'])
+        return case_id
     else:
-        return True
+        logging.error('TheHive: {}/{}'.format(response.status_code, response.text))
